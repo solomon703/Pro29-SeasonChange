eb5293af696d9074053dd65bc1813be152906c99
rajuboyella1753 <rajuboyella737@gmail.com>
2024-08-24 22:00:38 +0530

    New-Files

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Project 29 | Season Changing Page
+
+![ima](./images/mainPic.png)
+
+|Name|Project|Date|
+|---|---|---|
+|Raju|29|24/08/2024|
---
+++ script.js
@@ -0,0 +1,17 @@
+let image = document.getElementById("image");
+function spring(){
+  image.src = "./images/spring.png";
+  document.body.style.backgroundColor = "silver";
+}
+function summer(){
+  image.src = "./images/summer.png";
+  document.body.style.backgroundColor = "grey";
+}
+function autum(){
+  image.src = "./images/autum.png";
+  document.body.style.backgroundColor = "silver";
+}
+function winter(){
+  image.src = "./images/winter.png";
+  document.body.style.backgroundColor = "grey";
+}
